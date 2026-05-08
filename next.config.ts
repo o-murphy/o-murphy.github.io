28dfc9314c356e003ffebd03a5d596339fe50f2b
o-murphy <thehelixpg@gmail.com>
2026-05-08 18:34:59 +0300

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // Додайте для GitHub Pages
+  output: 'export',
+  basePath: '/portfolio',  // ← Додайте це - назва вашого репозиторію
+  assetPrefix: '/portfolio',  // ← Додайте це для статичних ассетів
   allowedDevOrigins: ['192.168.6.162', 'localhost'],
   
   images: {
-    unoptimized: true,  // Необхідно для статичного експорту
+    unoptimized: true,
   },
   
   webpack: (config) => {
